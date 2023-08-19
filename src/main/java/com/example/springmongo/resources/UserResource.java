04530d2632cbfdafc830894f2113d31f7dc21919
Wedson <wedsonsilva025@gmail.com>
2023-08-18 23:03:45 -0300

    obtendo um usuario por id

--- src/main/java/com/example/springmongo/resources/UserResource.java
+++ src/main/java/com/example/springmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,11 @@ public class UserResource {
 		List<UserDto>  listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDto> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDto(obj));
+	}
+	
 }

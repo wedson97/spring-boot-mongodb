a1e6b87d7266a01e0d0260f8fde822a1d6af0dd4
Wedson <wedsonsilva025@gmail.com>
2023-08-20 21:45:41 -0300

    obtendo um post por id

---
+++ src/main/java/com/example/springmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.example.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springmongo.domanin.Post;
+import com.example.springmongo.repository.PostRepository;
+import com.example.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
